95f2cead7ce76be81629cc8055c968a2f3bb6eda
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-02-01 22:31:58 -0300

    Primeiro prompt

--- src/gpt/use-cases/orthography.use-cases.ts
+++ src/gpt/use-cases/orthography.use-cases.ts
@@ -1,12 +1,30 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
 
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  };
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: 'Eres un asistente muy útil.',
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(completion);
+
+  return completion.choices[0];
 };
